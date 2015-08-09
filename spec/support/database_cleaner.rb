020c4a57af66f07a21a5218d7d17b14626a8ddb6
David Verhasselt <david@crowdway.com>
2015-08-09 18:22:47 +0300

    Add some RSpec utils for better testing

---
+++ spec/support/database_cleaner.rb
@@ -0,0 +1,41 @@
+module DatabaseCleanerUtils
+
+  attr :database_cleaner_strategy
+
+  def set_database_cleaner_strategy(strategy)
+    DatabaseCleaner.strategy = strategy
+    @database_cleaner_strategy = strategy
+  end
+
+end
+
+# Thank you Avdi - http://devblog.avdi.org/2012/08/31/configuring-database_cleaner-with-rails-rspec-capybara-and-selenium/
+RSpec.configure do |config|
+
+  config.include DatabaseCleanerUtils
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    set_database_cleaner_strategy(:transaction)
+  end
+
+  config.before(:each, :truncate) do
+    set_database_cleaner_strategy(:truncation)
+  end
+
+  config.before(:each, :js) do
+    set_database_cleaner_strategy(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+end
---
+++ spec/support/rspec_utils.rb
@@ -0,0 +1,26 @@
+module RSpecUtils
+
+  def outside_transaction
+    if RSpec.configuration.use_transactional_fixtures || database_cleaner_strategy == :transaction
+      raise <<-ERROR.strip_heredoc
+        Using this method doesn't work very well if DatabaseCleaner (or RSpec) uses transactional fixtures.
+        Consider setting the DatabaseCleaner strategy to :truncation.
+      ERROR
+    end
+
+    Thread.new do
+      ActiveRecord::Base.connection_pool.with_connection do
+        yield
+      end
+    end.join
+  end
+
+end
+
+RSpec.configure do |config|
+  config.include RSpecUtils
+
+  config.before(:each) do |example|
+    @example = example
+  end
+end
