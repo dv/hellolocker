64a3abc45b218805f33f656b7fee268f30a04e15
David Verhasselt <david@crowdway.com>
2015-08-21 22:23:42 +0300

    Make sure ShortLinkSequence.next returns a number

--- app/models/short_link_sequence.rb
+++ app/models/short_link_sequence.rb
@@ -3,7 +3,7 @@ class ShortLinkSequence
   def self.next
     result = ActiveRecord::Base.connection.execute("SELECT nextval('short_link_seq')")
 
-    result[0]["nextval"]
+    result[0]["nextval"].to_i
   end
 
 end
--- spec/models/short_link_sequence_spec.rb
+++ spec/models/short_link_sequence_spec.rb
@@ -3,6 +3,12 @@ require "rails_helper"
 RSpec.describe ShortLinkSequence do
 
   describe "#next" do
+    it "returns a number" do
+      num = ShortLinkSequence.next
+
+      expect(num).to be_a(Numeric)
+    end
+
     it "returns strictly rising numbers" do
       num1 = ShortLinkSequence.next
       num2 = ShortLinkSequence.next
