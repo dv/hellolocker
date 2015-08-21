3491f2f2fdb5dddbb90110a3820212da948c3849
David Verhasselt <david@crowdway.com>
2015-08-21 19:05:36 +0300

    Make ShortLinkSequence independent of ShortLink

--- app/models/short_link_sequence.rb
+++ app/models/short_link_sequence.rb
@@ -1,7 +1,7 @@
 class ShortLinkSequence
 
   def self.next
-    result = ShortLink.connection.execute("SELECT nextval('short_link_seq')")
+    result = ActiveRecord::Base.connection.execute("SELECT nextval('short_link_seq')")
 
     result[0]["nextval"]
   end
