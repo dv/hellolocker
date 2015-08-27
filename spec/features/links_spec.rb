63e2b64e5b6fc653e0a61d81401b49e884e7f619
David Verhasselt <david@crowdway.com>
2015-08-27 14:14:08 +0300

    Add Links feature specs

---
+++ spec/features/links_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.feature "Links" do
+
+  scenario "redirect to full URL", :js do
+    full_url = "http://example.com/"
+    label = "nanana"
+    short_item = create :item, url: full_url
+    short_link = create :link, item: short_item, label: label
+
+    visit "/#{label}"
+
+    expect(current_url).to eq(full_url)
+  end
+
+end
