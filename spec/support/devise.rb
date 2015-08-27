39110ae5f95fe985e7bdff042e052801a515bb68
David Verhasselt <david@crowdway.com>
2015-08-27 14:16:02 +0300

    Add devise helpers to specs

---
+++ spec/support/devise.rb
@@ -0,0 +1,18 @@
+module FeatureSessionHelpers
+  def create_user_and_login(*options)
+    user = create :user, *options
+
+    login_as(user)
+
+    user
+  end
+end
+
+RSpec.configure do |config|
+  config.include Warden::Test::Helpers
+  config.include FeatureSessionHelpers, type: :feature
+
+  config.before :suite do
+    Warden.test_mode!
+  end
+end
