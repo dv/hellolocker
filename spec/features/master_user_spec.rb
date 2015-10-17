d7b357a1cc6179a515632dbb4092180d3f23f75b
David Verhasselt <david@crowdway.com>
2015-10-17 14:58:32 +0300

    Use I18n

--- spec/features/master_user_spec.rb
+++ spec/features/master_user_spec.rb
@@ -4,20 +4,20 @@ RSpec.feature "Master User" do
 
   scenario "Can sign in" do
     visit root_path
-    click_on "Sign in"
+    click_on t("layouts.navigation.sign_in")
 
     fill_in "Password", with: Rails.application.secrets.master_password
     click_on "Log in"
 
-    expect(page).to have_content("Sign out")
+    expect(page).to have_content(t("layouts.navigation.sign_out"))
   end
 
   scenario "Can sign out" do
     single_user_login
 
-    click_link "Sign out"
+    click_link t("layouts.navigation.sign_out")
 
-    expect(page).to have_content("Sign in")
+    expect(page).to have_content(t("layouts.navigation.sign_in"))
   end
 
 end
